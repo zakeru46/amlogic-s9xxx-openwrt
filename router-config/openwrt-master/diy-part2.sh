09fa36ad25f1415b77b70d0ec0f9bfe400118cb3
ophub <ophub2020@gmail.com>
2022-05-16 21:25:18 +0800

    Add OpenWrt official-master

--- router-config/openwrt-master/diy-part2.sh
+++ router-config/openwrt-master/diy-part2.sh
@@ -3,7 +3,7 @@
 # https://github.com/ophub/amlogic-s9xxx-openwrt
 # Description: Automatically Build OpenWrt for Amlogic s9xxx tv box
 # Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/openwrt/openwrt / Branch: 21.02
+# Source code repository: https://github.com/openwrt/openwrt / Branch: master
 #========================================================================================================================
 
 # ------------------------------- Main source started -------------------------------
@@ -16,7 +16,7 @@ sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999
 
 # Set etc/openwrt_release
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
-echo "DISTRIB_SOURCECODE='openwrt.21.02'" >>package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCECODE='openwrt.master'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
 # sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
